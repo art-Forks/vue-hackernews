9640f9c16c4ab2be570a954440b2346729ea9536
art <partypeopleland@gmail.com>
2021-02-18 17:47:01 +0800

    ch4 ProgressBar unit test for start() and finish()

--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -1,14 +1,44 @@
-﻿import { shallowMount } from '@vue/test-utils'
+﻿import {shallowMount} from '@vue/test-utils'
 import ProgressBar from '../ProgressBar.vue'
+import Vue from "vue";
 
 describe('ProgressBar.vue', () => {
-  test('is hidden on initial render', () => {
-    const wrapper = shallowMount(ProgressBar)
-    expect(wrapper.classes()).toContain('hidden') // #A
-  })
-
   test('initializes with 0% width', () => {
     const wrapper = shallowMount(ProgressBar)
     expect(wrapper.element.style.width).toBe('0%') // #A
+  })
+
+  test('displays the bar when start is called', async () => {
+    const wrapper = shallowMount(ProgressBar)
+    expect(wrapper.classes()).toContain('hidden')
+    wrapper.vm.start()
+    await Vue.nextTick()
+    expect(wrapper.classes()).not.toContain('hidden')
+  })
+
+  test('sets the bar to 100% width when finish is called', async () => {
+    const wrapper = shallowMount(ProgressBar)
+    wrapper.vm.start()
+    wrapper.vm.finish()
+    await Vue.nextTick()
+    expect(wrapper.element.style.width).toBe('100%')
+  })
+
+
+  test('hides the bar when finish is called', async () => {
+    const wrapper = shallowMount(ProgressBar)
+    wrapper.vm.start()
+    wrapper.vm.finish()
+    await Vue.nextTick()
+    expect(wrapper.classes()).toContain('hidden')
+  })
+
+
+  test('resets to 0% width when start is called', async () => {
+    const wrapper = shallowMount(ProgressBar)
+    wrapper.vm.finish()
+    wrapper.vm.start()
+    await Vue.nextTick()
+    expect(wrapper.element.style.width).toBe('0%')
   })
 })
